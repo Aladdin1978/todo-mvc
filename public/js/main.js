503f67766be9ea592bc0d41503910f4793e291fc
Alladeen Aghel <aladdin.aghel@gmail.com>
2023-06-25 12:43:05 +0100

    added public/js/Main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,63 @@
 const deleteBtn = document.querySelectorAll(".del");
 const todoItem = document.querySelectorAll("span.not");
 const todoComplete = document.querySelectorAll("span.completed");
+
+Array.from(deleteBtn).forEach((el) => {
+  el.addEventListener("click", deleteTodo);
+});
+
+async function deleteTodo() {
+  const todoID = this.parentNode.dataset.id;
+
+  try {
+    const response = await fetch("todos/deleteTodo", {
+      method: "delete",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        todoIdFromJSFile: todoId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function markComplete(){
+  const todoId = this.parentNode.dataset.id
+  try{
+    const response = await fetch('todos/markComplete',{
+      method: 'put',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        'todoIdFromJSFile: todoId'
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+async function markIncomplete(){
+  const todoID = this.parentNode.dataset.id
+  try{
+    const response = await fetch('todos/markIncomplete', {
+      method: 'put',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        'todoIdFromJSFile': todoId
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  }catch(err){
+    console.log(err)
+  }
+}
